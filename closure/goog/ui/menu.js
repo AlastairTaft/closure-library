f0ea1ddf5d5fc39b5010a3436922b9b77207523c
AlastairTaft <AlastairTaft@users.noreply.github.com>
2015-03-15 20:52:37 +1100

    Use goog.base instead
    goog.ui.Menu.base is not compatible with advanced compilation

--- closure/goog/ui/menu.js
+++ closure/goog/ui/menu.js
@@ -423,7 +423,7 @@ goog.ui.Menu.prototype.decorateInternal = function(element) {
 
 /** @override */
 goog.ui.Menu.prototype.handleKeyEventInternal = function(e) {
-  var handled = goog.ui.Menu.base(this, 'handleKeyEventInternal', e);
+  var handled = this.base(this, 'handleKeyEventInternal', e);
   if (!handled) {
     // Loop through all child components, and for each menu item call its
     // key event handler so that keyboard mnemonics can be handled.
@@ -445,7 +445,7 @@ goog.ui.Menu.prototype.handleKeyEventInternal = function(e) {
 
 /** @override */
 goog.ui.Menu.prototype.setHighlightedIndex = function(index) {
-  goog.ui.Menu.base(this, 'setHighlightedIndex', index);
+  this.base(this, 'setHighlightedIndex', index);
 
   // Bring the highlighted item into view. This has no effect if the menu is not
   // scrollable.

1c1093b991bc14f2413b807f8ba34f3c2efd388a
Alastair Taft <ali.c.taft@gmail.com>
2015-03-18 15:18:50 +1100

    Updated the entry point file to load the closure library in the current context.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+var fs = require('fs');
+var path = require('path');
+var vm = require('vm');
+
 module.exports = function(){
-	console.log("Hello world");
+	
+	var basePath = path.resolve(__dirname, 'closure', 'goog', 'base.js');
+	vm.runInThisContext.call(
+      this, fs.readFileSync(basePath), basePath);
+	
 };
